b4c5dc5da3065cddf26b0c49869ccf9e2fdfe05d
daphne Fang <daphneys.fang@gmail.com>
2020-02-06 23:51:55 -0800

    Updates

---
+++ precache-manifest.8c98297e45a5249e1994d5aeae3b7922.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7ecb0b9ae19a00da5554a46ddf6bf78",
+    "url": "/google-book-search/index.html"
+  },
+  {
+    "revision": "84b1029f18c4f432620d",
+    "url": "/google-book-search/static/css/main.319950e3.chunk.css"
+  },
+  {
+    "revision": "b54273afa9d418c79e67",
+    "url": "/google-book-search/static/js/2.05f639b8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/google-book-search/static/js/2.05f639b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84b1029f18c4f432620d",
+    "url": "/google-book-search/static/js/main.d18b4418.chunk.js"
+  },
+  {
+    "revision": "4f64f4a172b1e685b8ad",
+    "url": "/google-book-search/static/js/runtime-main.b3c87af4.js"
+  }
+]);
